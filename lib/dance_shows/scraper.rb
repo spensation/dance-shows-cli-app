67eb81308d7b2e58b127921d7b3f0265037f632a
spensation <spensation@gmail.com>
2018-03-09 15:06:00 -0800

    cli working again

--- lib/dance_shows/scraper.rb
+++ lib/dance_shows/scraper.rb
@@ -6,7 +6,7 @@ class DanceShows::Scraper
       show = DanceShows::Show.new
 
 
-      show.name = show_div.search('h2 a').first.attributes['title'].value
+      show.name = show_div.search('h2 a').first.attributes['title']
       show.venue = show_div.search('.tribe-events-venue-details').children[0..2].text.gsub("\n", "").gsub("\t", "")
       show.cost = show_div.search(".tribe-events-event-cost").children.text.strip
       show.date = show_div.search(".tribe-event-schedule-details").children.text.strip
